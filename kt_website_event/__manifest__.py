84d98ebc98a6bcddd56dae9d318b52605733b4e1
Karan <karan@strategicdimensions.co.za>
2018-10-13 17:37:55 +0530

    work on dynamic menu

--- kt_website_event/__manifest__.py
+++ kt_website_event/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Website Events Changes',
-    'version': '11.0.3',
+    'version': '11.0.4',
     'sequence':6,
     'website': 'https://www.odoo.com/page/events',
     'category': 'Tools',
--- kt_website_event/models/website_event.py
+++ kt_website_event/models/website_event.py
@@ -13,3 +13,11 @@ class event_ticket(models.Model):
     _inherit = 'event.event.ticket'
 
     is_free = fields.Boolean('Is Free')
+
+
+class Dynamic_menu(models.Model):
+    _name = 'dynamic.menu'
+
+    name = fields.Char(string="Main Menu")
+    parent_id = fields.Many2one('dynamic.menu', 'name')
+    menu_url = fields.Char(string="Url")
