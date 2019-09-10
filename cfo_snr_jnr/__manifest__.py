e99271dcc65603636a4e84860be87d34cd40ab40
Karan Nanda <karan.acespritech@gmail.com>
2019-09-10 16:10:58 +0530

    changes in timetable page

--- cfo_snr_jnr/__manifest__.py
+++ cfo_snr_jnr/__manifest__.py
@@ -20,7 +20,7 @@
 
 {
     'name': 'CFO Senior Junior',
-    'version': '11.0.15',
+    'version': '11.0.16',
     'category': 'Website',
     'summary': 'CFO Senior and Junior Competition',
     'description': """CFO Senior and Junior Competition.""",
