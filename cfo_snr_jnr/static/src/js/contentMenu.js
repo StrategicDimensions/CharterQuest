235224f32b7918565aaee351b365caeff08cab1c
Rahul <rahul.acespritech@gmail.com>
2019-11-22 18:47:49 +0530

    Add coockies popup on browser

--- cfo_snr_jnr/static/src/js/contentMenu.js
+++ cfo_snr_jnr/static/src/js/contentMenu.js
@@ -1,6 +1,7 @@
 odoo.define('cfo_snr_jnr.contentMenu', function (require) {
 
 	var contentMenu = require('website.contentMenu');
+
 	contentMenu.EditMenuDialog.include({
 		start: function () {
 	        var r = this._super.apply(this, arguments);
