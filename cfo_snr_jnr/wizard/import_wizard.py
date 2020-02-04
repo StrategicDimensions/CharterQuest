c4972ff89ddfddc823f3547bccae339c36354ba5
Rahul <rahul.acespritech@gmail.com>
2020-02-04 12:25:17 +0530

    Solve issue:(1)changes into template for reminder to a Debit Order entry 2 days before (2)fixing template Sending Tax Invoice to Student

--- cfo_snr_jnr/wizard/import_wizard.py
+++ cfo_snr_jnr/wizard/import_wizard.py
@@ -19,58 +19,50 @@ class YourWizard(models.TransientModel):
         #     # csv module expect utf-8, see http://docs.python.org/2/library/csv.html
         #     csv_data = csv_data.decode(encoding).encode('utf-8')
 
-        csv_iterator = pycompat.csv_reader(io.BytesIO(csv_data))
-        # csv_iterator = pycompat.csv_reader(csv_data)
-        print("\n\n\n\n\n\n===============csv_iterator=========",csv_iterator)
-        for line in csv_iterator:
-            if not line:
+        res_user_file = open(csv_data, "rb")
+        res_user_reader = csv.reader(res_user_file)
+        for row, line in enumerate(res_user_reader):
+            if not row:
                 continue
             else:
-                print("\n\n\n\n\n=========line[0]======", line[0])
-                print("\n\n\n\n\n=========line[1]======", line[1])
-                print("\n\n\n\n=========line[3]=========", line[3])
-                print("\n\n\n\n\n=============line[48]=========", line[48])
-                print("\n\n\n\n\n=============line[49]=========", line[49])
+                partner_id = self.env['res.partner'].search([('email','=',line[4])])
 
-                # partner_id = self.env['res.partner'].search([('email','=',line[4])])
-
-                # if partner_id:
-                #
-                #     update_partner = partner_id.write({'name': line[0],
-                #                             'phone':line[1],
-                #                             'mobile': line[2],
-                #                             'email': line[4],
-                #                             'website': line[5],
-                #                             'student_number': line[6],
-                #                             'street2': line[7] or line[8] or line[9] or line[10],
-                #                             'street': line[11] or line[12] or line[13] or line[14],
-                #                             'city': line[15] or line[16] or line[17] or line[18],
-                #                             'state_id': line[19] or line[20] or line[21] or line[22],
-                #                             'country_id': line[23] or line[24] or line[25] or line[26],
-                #                             'zip': line[27] or line[28] or line[29] or line[30],
-                #                             'event_type_id': line[31],
-                #                             'vat_no_comp': line[32],
-                #                             'cq_password': line[33],
-                #                             'prof_password': line[34],
-                #                             'title': line[35],
-                #                             'dob': line[36] or line[37],
-                #                             'prof_body_id': line[38],
-                #                             'student_company': line[39],
-                #                             'lang': line[40],
-                #                             'cfo_user': line[41],
-                #                             'customer': line[43],
-                #                             'idpassport': line[44],
-                #                             'property_stock_customer': line[45],
-                #                             'property_stock_supplier': line[46],
-                #                             'opt_out': line[47],
-                #                             'is_campus': line[54],
-                #                             'property_account_payable_id': line[50] or line[51] or line[52],
-                #                             'property_account_receivable_id': line[48] or line[49] or line[53],
-                #                             'student_number_allocated': line[57],
-                #                             'previous_student': line[58],
-                #                             'current_student': line[59],
-                #                             'examwritten': line[60],
-                #                             })
+                if partner_id:
+                    update_partner = partner_id.write({'name': line[0],
+                                            'phone':line[1],
+                                            'mobile': line[2],
+                                            'email': line[4],
+                                            'website': line[5],
+                                            'student_number': line[6],
+                                            'street2': line[7] or line[8] or line[9] or line[10],
+                                            'street': line[11] or line[12] or line[13] or line[14],
+                                            'city': line[15] or line[16] or line[17] or line[18],
+                                            'state_id': line[19] or line[20] or line[21] or line[22],
+                                            'country_id': line[23] or line[24] or line[25] or line[26],
+                                            'zip': line[27] or line[28] or line[29] or line[30],
+                                            'event_type_id': line[31],
+                                            'vat_no_comp': line[32],
+                                            'cq_password': line[33],
+                                            'prof_password': line[34],
+                                            'title': line[35],
+                                            'dob': line[36] or line[37],
+                                            'prof_body_id': line[38],
+                                            'student_company': line[39],
+                                            'lang': line[40],
+                                            'cfo_user': line[41],
+                                            'customer': line[43],
+                                            'idpassport': line[44],
+                                            'property_stock_customer': line[45],
+                                            'property_stock_supplier': line[46],
+                                            'opt_out': line[47],
+                                            'is_campus': line[54],
+                                            'property_account_payable_id': line[50] or line[51] or line[52],
+                                            'property_account_receivable_id': line[48] or line[49] or line[53],
+                                            'student_number_allocated': line[57],
+                                            'previous_student': line[58],
+                                            'current_student': line[59],
+                                            'examwritten': line[60],
+                                            })
 
 
 
